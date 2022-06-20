f46532475838d6462ff359a88a1502a2ac1d168f
Sergey Tumanov <tumanser@gmail.com>
2022-06-20 18:31:05 +0300

    Amend watch

--- src/ui/webpack.config.js
+++ src/ui/webpack.config.js
@@ -1,19 +1,26 @@
 const path = require('path');
 const CopyPlugin = require("copy-webpack-plugin");
 
-const TARGET = path.resolve(__dirname, '../../build/ui/static')
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'contact-manager.js',
-        path: TARGET,
-    },
-    plugins: [
-        new CopyPlugin({
-            patterns: [
-                { from: "static", to: TARGET },
-            ],
-        }),
-    ],
+module.exports = env => {
+    let TARGET = path.resolve(__dirname, '../../build/ui/static')
+    if (env.watch) {
+        TARGET = path.resolve(__dirname, '../../build/resources/main/static')
+    }
+    console.log(`TARGET: ${TARGET}`);
+
+    return {
+        entry: './src/index.js',
+        output: {
+            filename: 'contact-manager.js',
+            path: TARGET,
+        },
+        plugins: [
+            new CopyPlugin({
+                patterns: [
+                    { from: "static", to: TARGET },
+                ],
+            }),
+        ],
+    };
 };
