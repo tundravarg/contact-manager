3ccd2781618f1bf2074907b1882336db91db913d
Sergey Tumanov <tumanser@gmail.com>
2022-06-20 14:55:33 +0300

    More complex test data

--- src/sql/contacts.sql
+++ src/sql/contacts.sql
@@ -1,3 +1,6 @@
-SELECT *
-FROM "contacts"
+SELECT c.*, v.*
+FROM "contacts" AS c
+LEFT JOIN contact_values_link AS cvl ON cvl.contact_id = c.id
+LEFT JOIN contact_values AS v ON v.id = cvl.value_id
+ORDER BY c.name, v.type, v.subtype, v.value
 ;
--- src/sql/test-data.sql
+++ src/sql/test-data.sql
@@ -4,3 +4,51 @@ INSERT INTO "contacts" (
     ('Sergey', 'Sergey', 'Tumanov', 'Gennadyevich'),
     ('IvanP', 'Ivan', 'Petrov', 'Ignatyevich'),
     ('Director', 'Andrey', 'Repin', 'Mihailovich')
+;
+
+
+INSERT INTO contact_values (
+    type, subtype, value
+) VALUES
+    ('PHONE', 'PERSONAL', '+79215576152'),
+    ('EMAIL', 'PERSONAL', 'tumanser@gmail.com'),
+    ('VK', NULL, 'https://vk.com/tumanser')
+;
+
+INSERT INTO contact_values_link (
+    contact_id, value_id
+) VALUES
+    (
+        (SELECT id FROM contacts WHERE name = 'Sergey'),
+        (SELECT seq - 2 FROM sqlite_sequence WHERE name = 'contact_values')
+    ),
+    (
+        (SELECT id FROM contacts WHERE name = 'Sergey'),
+        (SELECT seq - 1 FROM sqlite_sequence WHERE name = 'contact_values')
+    ),
+    (
+        (SELECT id FROM contacts WHERE name = 'Sergey'),
+        (SELECT seq - 0 FROM sqlite_sequence WHERE name = 'contact_values')
+    )
+;
+
+INSERT INTO contact_values (
+    type, subtype, value
+) VALUES
+      ('PHONE', 'PERSONAL', '+1123456789'),
+      ('PHONE', 'WORK', '+7123456789'),
+      ('EMAIL', 'WORK', 'ivanp@gmail.com')
+;
+
+INSERT INTO contact_values_link (
+    contact_id, value_id
+) VALUES
+      (
+          (SELECT id FROM contacts WHERE name = 'IvanP'),
+          (SELECT seq - 1 FROM sqlite_sequence WHERE name = 'contact_values')
+      ),
+      (
+          (SELECT id FROM contacts WHERE name = 'IvanP'),
+          (SELECT seq - 0 FROM sqlite_sequence WHERE name = 'contact_values')
+      )
+;
