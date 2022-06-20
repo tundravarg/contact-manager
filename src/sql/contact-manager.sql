0218072b22fe7008b836c6de23817939413ccc57
Sergey Tumanov <tumanser@gmail.com>
2022-06-20 10:11:29 +0300

    Create tables

---
+++ src/sql/contact-manager.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS contacts;
+CREATE TABLE contacts (
+    id integer PRIMARY KEY,
+    name text,
+    primary_name text,
+    secondary_name text,
+    middle_name text
+);
+
+DROP TABLE IF EXISTS contact_values;
+CREATE TABLE contact_values (
+    id integer PRIMARY KEY,
+    "type" text,
+    subtype text,
+    "value" text
+);
+
+DROP TABLE IF EXISTS contact_values_link;
+CREATE TABLE contact_values_link (
+    contact_id integer,
+    value_id integer,
+    FOREIGN KEY (contact_id) REFERENCES contacts (id),
+    FOREIGN KEY (value_id) REFERENCES contact_values (id)
+);
