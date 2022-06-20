ced5679fcac423339f23685f3aad52ebe287bc64
Sergey Tumanov <tumanser@gmail.com>
2022-06-20 11:48:10 +0300

    Fill test data

---
+++ src/sql/test-data.sql
@@ -0,0 +1,6 @@
+INSERT INTO "contacts" (
+    "id", "name", "primary_name", "secondary_name", "middle_name"
+) VALUES
+    (1, 'Sergey', 'Sergey', 'Tumanov', 'Gennadyevich'),
+    (2, 'IvanP', 'Ivan', 'Petrov', 'Ignatyevich'),
+    (3, 'Director', 'Andrey', 'Repin', 'Mihailovich')
