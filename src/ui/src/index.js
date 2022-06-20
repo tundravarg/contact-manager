733cefdf42c94bde53dcac322dab5a40fce8d74c
Sergey Tumanov <tumanser@gmail.com>
2022-06-20 18:43:13 +0300

    Request from UI to API

--- src/ui/src/index.js
+++ src/ui/src/index.js
@@ -2,3 +2,13 @@ import $ from "jquery"
 
 console.log('1.0.0')
 $('#version').html('1.0.0')
+
+$.get('/api/contacts', data => {
+    const contactsElement = $('#contacts');
+    data.forEach(contact => {
+        console.log(contact.name);
+        let contactElement = $('<p>');
+        contactElement.text(`${contact.name}: ${contact.primaryName} ${contact.middleName} ${contact.secondaryName}`);
+        contactsElement.append(contactElement);
+    });
+});
