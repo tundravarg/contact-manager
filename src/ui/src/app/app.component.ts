18c184b62e8d621f9e0d82c79fae006ca0f489c5
Sergey Tumanov <tumanser@gmail.com>
2022-06-21 11:32:22 +0300

    Restore main page

--- src/ui/src/app/app.component.ts
+++ src/ui/src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ui';
+    title = 'Contact Manager';
+    version = '1.0.0'
 }
